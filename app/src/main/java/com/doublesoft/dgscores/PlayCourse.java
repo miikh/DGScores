8f73cecc4ff1859e5d91f13e51406d96400d3362
yonejn <jouni.nousiainen@gmail.com>
2017-04-08 11:01:26 +0300

    korjailua

---
+++ app/src/main/java/com/doublesoft/dgscores/PlayCourse.java
@@ -0,0 +1,15 @@
+package com.doublesoft.dgscores;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class PlayCourse extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play_course);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+}
